2e1f9001ba5d19cbac9df7ca64c9cdb025b75da8
Oh Hyeon Ho <jumpok821@gmail.com>
2021-01-13 23:59:27 +0900

    iocp ing~

--- Server/CartoonWarServer/CartoonWarServer/server.cpp
+++ Server/CartoonWarServer/CartoonWarServer/server.cpp
@@ -1,69 +1,43 @@
 #include "server.h"
 
-char messageBuffer[MAX_BUFFER];
-WSABUF wsabuf;
-SOCKET client_socket;
-
-void CALLBACK send_complete(DWORD err, DWORD bytes, LPWSAOVERLAPPED over, DWORD flags)
-{
-    if (bytes > 0) printf("TRACE - Send message : %s (%d bytes)\n", messageBuffer, bytes);
-    else {
-        closesocket(client_socket);
-        return;
-    }
-
-    wsabuf.len = MAX_BUFFER;
-    ZeroMemory(over, sizeof(*over));
-    int ret = WSARecv(client_socket, &wsabuf, 1, NULL, &flags, over, recv_complete);
-
-}
-
-void CALLBACK recv_complete(DWORD err, DWORD bytes, LPWSAOVERLAPPED over, DWORD flags)
-{
-    if (bytes > 0) {
-        messageBuffer[bytes] = 0;
-        cout << "TRACE - Receive message : " << messageBuffer << "(" << bytes << " bytes)\n";
-    }
-    else
-    {
-        closesocket(client_socket);
-        return;
-    }
-    wsabuf.len = bytes;
-    ZeroMemory(over, sizeof(*over));   //오버랩트 초기화 해주고 사용하기
-    int ret = WSASend(client_socket, &wsabuf, 1, NULL, NULL, over, send_complete);
-
-}
-
 void mainServer()
 {
     WSADATA WSAData;
-    WSAStartup(MAKEWORD(2, 0), &WSAData);
+    WSAStartup(MAKEWORD(2, 2), &WSAData);
 
     SOCKET listenSocket = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
 
     SOCKADDR_IN serverAddr;
-    memset(&serverAddr, 0, sizeof(SOCKADDR_IN));
+    ZeroMemory(&serverAddr, sizeof(serverAddr));
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(SERVER_PORT);
-    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_addr.S_un.S_addr = htonl(INADDR_ANY);
 
-    ::bind(listenSocket, (sockaddr*)&serverAddr, sizeof(serverAddr));
-    listen(listenSocket, 5);
+    ::bind(listenSocket, (sockaddr*)&serverAddr, sizeof(serverAddr)); // 그냥 바인드 쓰면 C++11에 있는 내장함수가 써짐
+    listen(listenSocket, SOMAXCONN);
 
-    SOCKADDR_IN client_addr;
-
-    WSAOVERLAPPED overlapped;
+    SOCKADDR_IN clientAddr;
+    ZeroMemory(&clientAddr, sizeof(clientAddr));
+    int c_AddrSize = sizeof(clientAddr);
 
     while (true) {
-        int addr_size = sizeof(client_addr);
-        client_socket = accept(listenSocket, (sockaddr*)&client_addr, &addr_size);
+        SOCKET clientSocket = accept(listenSocket, (sockaddr*)&clientAddr, &c_AddrSize);
 
-        wsabuf.buf = messageBuffer;
-        wsabuf.len = MAX_BUFFER;
-        DWORD flags = 0;
-        ZeroMemory(&overlapped, sizeof(overlapped));
-        int ret = WSARecv(client_socket, &wsabuf, 1, NULL, &flags, &overlapped, recv_complete);
+        int user_id = current_User_ID++;
+        current_User_ID %= MAX_USER; // 10 넘어 갔을때 걸러내는 용도
+
+        g_clients[user_id].m_id = user_id;
+        g_clients[user_id].m_prev_size = 0; // 버퍼 0으로 초기화
+        g_clients[user_id].m_recv_over.op = OP_RECV; // 오버랩 구조체에 받는걸로 설정
+        ZeroMemory(&g_clients[user_id].m_recv_over.over, sizeof(g_clients[user_id].m_recv_over.over)); // 오버랩 구조체 초기화
+        g_clients[user_id].m_recv_over.wsabuf.buf = g_clients[user_id].m_recv_over.io_buf; // WSA 버퍼 위치 설정
+        g_clients[user_id].m_recv_over.wsabuf.len = MAX_BUF_SIZE; // WSA버퍼 크기 설정
+        g_clients[user_id].m_socket = clientSocket;
+        g_clients[user_id].m_x = rand() % WORLD_WIDTH;
+        g_clients[user_id].m_x = rand() % WORLD_HEIGHT;
+
+        WSARecv(clientSocket, &g_clients[user_id].m_recv_over.wsabuf, 1, NULL, 
+            &flags, g_clients[user_id].m_recv_over.over, NULL);
 
     }
     closesocket(listenSocket);
