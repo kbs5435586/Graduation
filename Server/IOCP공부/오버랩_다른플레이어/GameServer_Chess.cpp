49af390acd98838a5f518fa3532ecb8c9d85d8d9
Oh Hyeon Ho <jumpok821@gmail.com>
2021-01-16 21:36:22 +0900

    API Client packet routine

--- Server/IOCP공부/오버랩_다른플레이어/GameServer_Chess.cpp
+++ Server/IOCP공부/오버랩_다른플레이어/GameServer_Chess.cpp
@@ -19,6 +19,7 @@ SOCKET s_socket; // 클라이언트와 연결할 소켓
 string client_ip;
 constexpr int BUF_SIZE = 1024;
 constexpr short PORT = 3500;
+int g_myid;
 
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
@@ -49,7 +50,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, // H는 핸들,
     HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_GAMESERVERCHESS));
 
     WSADATA WSAData;
-    WSAStartup(MAKEWORD(2, 0), &WSAData);
+    WSAStartup(MAKEWORD(2, 2), &WSAData);
     s_socket = WSASocket(AF_INET, SOCK_STREAM, 0, NULL, 0, WSA_FLAG_OVERLAPPED);
 
     MSG msg;
@@ -287,4 +288,102 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
     }
    
     return (INT_PTR)FALSE;
+}
+
+void ProcessPacket(char* ptr)
+{
+    static bool first_time = true;
+    switch (ptr[1])
+    {
+    case SC_PACKET_LOGIN_OK:
+    {
+        sc_packet_login_ok* my_packet = reinterpret_cast<sc_packet_login_ok*>(ptr);
+        g_myid = my_packet->id;
+        avatar.move(my_packet->x, my_packet->y);
+        //g_left_x = my_packet->x - (SCREEN_WIDTH / 2);
+        //g_top_y = my_packet->y - (SCREEN_HEIGHT / 2);
+        avatar.show();
+    }
+    break;
+
+    case SC_PACKET_ENTER:
+    {
+        sc_packet_enter* my_packet = reinterpret_cast<sc_packet_enter*>(ptr);
+        int id = my_packet->id;
+
+        if (id == g_myid) {
+            avatar.move(my_packet->x, my_packet->y);
+            //g_left_x = my_packet->x - (SCREEN_WIDTH / 2);
+            //g_top_y = my_packet->y - (SCREEN_HEIGHT / 2);
+            avatar.show();
+        }
+        else {
+            if (id < NPC_ID_START)
+                npcs[id] = OBJECT{ *pieces, 64, 0, 64, 64 };
+            else
+                npcs[id] = OBJECT{ *pieces, 0, 0, 64, 64 };
+            strcpy_s(npcs[id].name, my_packet->name);
+            npcs[id].set_name(my_packet->name);
+            npcs[id].move(my_packet->x, my_packet->y);
+            npcs[id].show();
+        }
+    }
+    break;
+    case SC_PACKET_MOVE:
+    {
+        sc_packet_move* my_packet = reinterpret_cast<sc_packet_move*>(ptr);
+        int other_id = my_packet->id;
+        if (other_id == g_myid) {
+            avatar.move(my_packet->x, my_packet->y);
+            //g_left_x = my_packet->x - (SCREEN_WIDTH / 2);
+            //g_top_y = my_packet->y - (SCREEN_HEIGHT / 2);
+        }
+        else {
+            if (0 != npcs.count(other_id))
+                npcs[other_id].move(my_packet->x, my_packet->y);
+        }
+    }
+    break;
+
+    case SC_PACKET_LEAVE:
+    {
+        sc_packet_leave* my_packet = reinterpret_cast<sc_packet_leave*>(ptr);
+        int other_id = my_packet->id;
+        if (other_id == g_myid) {
+            avatar.hide();
+        }
+        else {
+            if (0 != npcs.count(other_id))
+                npcs[other_id].hide();
+        }
+    }
+    break;
+    default:
+        printf("Unknown PACKET type [%d]\n", ptr[1]);
+    }
+}
+
+void process_data(char* net_buf, size_t io_byte)
+{
+    char* ptr = net_buf;
+    static size_t in_packet_size = 0;
+    static size_t saved_packet_size = 0;
+    static char packet_buffer[BUF_SIZE];
+
+    while (0 != io_byte) {
+        if (0 == in_packet_size) in_packet_size = ptr[0];
+        if (io_byte + saved_packet_size >= in_packet_size) {
+            memcpy(packet_buffer + saved_packet_size, ptr, in_packet_size - saved_packet_size);
+            ProcessPacket(packet_buffer);
+            ptr += in_packet_size - saved_packet_size;
+            io_byte -= in_packet_size - saved_packet_size;
+            in_packet_size = 0;
+            saved_packet_size = 0;
+        }
+        else {
+            memcpy(packet_buffer + saved_packet_size, ptr, io_byte);
+            saved_packet_size += io_byte;
+            io_byte = 0;
+        }
+    }
 }
